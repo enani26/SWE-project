0ff59f3461c965cd0e6d1745b03c5b119beda415
MazenFouad <Mazenfouad02@gmail.com>
2023-10-20 19:10:15 +0300

    .

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<?php
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+<div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
+        <div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
+            <a href="about.php" style="left: 646px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">ABOUT</a>
+            <a href="store.php" style="left: 800px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">STORE</a>
+            <a href="contact.php" style="left: 949px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">CONTACT</a>
+            <a href="home.php" style="left: 513px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">HOME</a>
+            <div style="width: 31.99px; height: 28.86px; left: 1116px; top: 8.57px; position: absolute; color: #7F96B3; font-size: 25px;"> 🛒</div>
+            <div style ="left: 0px; top: 0px; position: absolute; color: #7F96B3; font-size: 48px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">MISERY</div>
+            <div style="width: 33px; height: 32px; left: 1177.50px; top: 9px; position: absolute; color: #7F96B3;font-size: 25px;"> ♡</div>
+            <a href="register.php" style="left: 1282px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">REGISTER</a>
+        </div>
---
+++ views/pages/contactus.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="contact.css">
+    <title>contact</title>
+</head>
+<body>
+   
+    
+    <div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
+        <div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
+            <a href="about.html" style="left: 646px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">ABOUT</a>
+            <a href="store.html" style="left: 800px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">STORE</a>
+            <a href="contact.html" style="left: 949px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">CONTACT</a>
+            <a href="home.html" style="left: 513px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">HOME</a>
+            <div style="width: 31.99px; height: 28.86px; left: 1116px; top: 8.57px; position: absolute; color: #7F96B3; font-size: 25px;"> 🛒</div>
+            <div style ="left: 0px; top: 0px; position: absolute; color: #7F96B3; font-size: 48px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">MISERY</div>
+            <div style="width: 33px; height: 32px; left: 1177.50px; top: 9px; position: absolute; color: #7F96B3;font-size: 25px;"> ♡</div>
+            <a href="register.html" style="left: 1282px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">REGISTER</a>
+        </div>
+    
+</div>
+        <div style="width: 1440px; height: 1024px; left: 11px; top: 14px; position: absolute;">
+            <div style="width: 1440px; height: 1024px; left: 0px; top: 0px; position: absolute;"></div>
+            <div style="width: 622px; height: 143px; left: 36px; top: 155px; position: absolute; color: white; font-size: 64px; font-family: MuseoModerno; font-weight: 400; word-wrap: break-word">GET IN TOUCH.</div>
+            <div style="width: 457px; height: 0px; left: 36px; top: 262.50px; position: absolute; border: 4px #7F96B3 solid"></div>
+            <div style="width: 607px; left: 36px; top: 271px; position: absolute; color: white; font-size: 24px; font-family: Montaga; font-weight: 400; word-wrap: break-word">"Connect with Misery. We're all ears for your questions and thoughts. Reach out today."</div>
+            <div style="width: 552px; height: 62px; left: 33px; top: 409px; position: absolute; background: #7F96B3; box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.75)"></div>
+            <div style="width: 552px; height: 62px; left: 33px; top: 530px; position: absolute; background: #7F96B3; box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.75)"></div>
+            <div style="width: 552px; height: 225px; left: 33px; top: 651px; position: absolute; background: #7F96B3; box-shadow: 7px 7px 5px rgba(0, 0, 0, 0.75)"></div>
+            <input type="text" placeholder="email" style="width: 421px; height: 35px; left: 40px; top: 420px; position: absolute; color: black; font-size: 24px; font-family: Montaga; font-weight: 400; word-wrap: break-word">Email:</input>
+            <input type="text" placeholder="name" style="left: 40px; top: 546px; position: absolute; color: black; font-size: 24px; font-family: Montaga; font-weight: 400; word-wrap: break-word">Name:</input>
+            <textarea style="left: 40px; top: 667px; position: absolute; color: black; font-size: 24px; font-family: Montaga; font-weight: 400; word-wrap: break-word">Description:</textarea>
+            <button type="submit" style="width: 257px; height: 57px; left: 154px; top: 923px; position: absolute; background: rgba(127, 150, 179, 0.60); box-shadow: 0px 0px 25px rgba(101, 153, 32, 0.25); border-radius: 40px; color: black; font-size: 24px; font-family: Montserrat; font-weight: 700; text-transform: lowercase; word-wrap: break-word">Submit</button>
+            
+        </div>
+        <img style="width: 1241px; height: 863px; left: 434px; top: 129px; position: absolute" src="C:\Users\Marwan.Enani\Desktop\Misery\images\c859f259ce99199e62553b5052d46e2f.png" />
+    </div>
+    
+</body>
+</html>
+
+<?php
+
+?>
