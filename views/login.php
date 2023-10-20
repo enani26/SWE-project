d2c223728f8332b7cb225d86708f944017af3ff7
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2023-10-20 19:13:03 +0300

    login

---
+++ views/login.php
@@ -0,0 +1,43 @@
+<!-- here will be the sign up form-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+        <div class="container">
+            <div class="box form-box">
+
+                <header>Sign in</header><br>
+                <form action="" method="post">
+                    <div class="field input">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" id="username" required>
+                    </div>
+
+                    <div class="field input">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" required>
+                    </div>
+
+                    <div class="field">
+                        
+                        <input type="submit" class="btn" name="submit" value="Login" required>
+                    </div>
+                    <div class="links">
+                        Don't have an account? <a href="register.html">Sign Up Now</a>
+
+                    </div>
+                </form>
+
+            
+
+
+        </div>
+        </div>
+</body>
+</html>
