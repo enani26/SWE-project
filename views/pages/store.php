38f5332d565bcb6ad1d8ca70a5229231c5723b9b
mohamed2106505 <140180824+mohamed2106505@users.noreply.github.com>
2023-10-21 19:13:33 +0300

    store

---
+++ views/pages/store.php
@@ -0,0 +1,74 @@
+<html>
+    <title>
+        Misery store
+    </title>
+<body>
+<div class="Desktop1" style="width: 100%; height: 100%; position: relative; background: #252124">
+    <div class="Line1" style="width: 1440.09px; height: 0px; left: 0px; top: 181px; position: absolute; transform: rotate(-0.65deg); transform-origin: 0 0; border: 2px #7F96B3 solid"></div>
+    <div class="Cta" style="width: 159px; height: 57px; left: 10px; top: 106px; position: absolute; background: rgba(127, 150, 179, 0.60); box-shadow: 0px 0px 25px rgba(101, 153, 32, 0.25); border-radius: 40px"></div>
+    <div class="SortBy" style="left: 39px; top: 118px; position: absolute; color: black; font-size: 25px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">sort by:</div>
+    <div class="Line3" style="width: 190.01px; height: 0px; left: -1px; top: 288.99px; position: absolute; transform: rotate(-0.60deg); transform-origin: 0 0; border: 1px #7F96B3  solid"></div>
+    <div class="Tops" style="left: 10px; top: 234px; position: absolute; color: white; font-size: 18px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">tops:</div>
+    <div class="Pants" style="left: 10px; top: 257px; position: absolute; color: white; font-size: 18px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">pants</div>
+    <div class="ViewAll" style="width: 80px; height: 23px; left: 10px; top: 211px; position: absolute; color: white; font-size: 18px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">view all:</div>
+    <div class="Line2" style="width: 190.01px; height: 0px; left: 3px; top: 204.99px; position: absolute; transform: rotate(-0.60deg); transform-origin: 0 0; border: 1px #7F96B3  solid"></div>
+    <div class="OffersAndDeals" style="left: 10px; top: 296px; position: absolute; text-align: center; color: white; font-size: 19px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">Offers and deals</div>
+    <div class="Sales" style="left: 3px; top: 336px; position: absolute; color:white; font-size: 19px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word"> sales:</div>
+    <div class="Line4" style="width: 799.03px; height: 0px; left: 200px; top: 192px; position: absolute; transform: rotate(90.50deg); transform-origin: 0 0; border: 1px #7F96B3 solid"></div>
+    <div class="Frame3" style="left: 235px; top: 197px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+        <div class="Picture1" style="width: 418px; height: 395px; background: #252124"></div>
+        <div class="BrownHoodie" style="left: 20px; top: 264px; position: absolute; text-align: center; color: #7F96B3; font-size: 25px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">Olive hoodie</div>
+        <div class="Rectangle16" style="width: 113px; height: 39px; left: 11px; top: 330px; position: absolute; background: #D9D9D9; border-radius: 40px"></div>
+        <div class="AddToCart" style="left: 30px; top: 338px; position: absolute; text-align: center; color: black; font-size: 16px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">add to cart</div>
+        <div class="IconHeartOutline" style="width: 52.84px; height: 53px; left: 345px; top: 321px; position: absolute">
+        <div class="Vector" style="width: 52.84px; height: 53px; left: 0px; top: 0px; position: absolute; opacity: 0; background: white">*</div>
+            <div class="Vector" style="width: 44.04px; height: 37.54px; left: 4.40px; top: 8.83px; position: absolute; background: transparent;color:white">*</div>
+        </div>
+        <img class="Unnamed3RemovebgPreview1" style="width: 297px; height: 238px; left: 47px; top: 0px; position: absolute" src="C:\xampp\htdocs\swe project\olive.png" />
+    </div>
+    <div class="Frame4" style="left: 939px; top: 197px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+        <div class="Picture1" style="width: 418px; height: 367px; background: #252124"></div>
+        <img class="Unnamed3RemovebgPreview1" style="width: 297px; height: 238px; left: 47px; top: 0px; position: absolute" src="C:\xampp\htdocs\swe project\skull.png" />
+        <div class="LivingDeathOversizedTShirt" style="left: 54px; top: 264px; position: absolute; text-align: center; color: #7F96B3; font-size: 25px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">skull  oversized t-shirt</div>
+        <div class="Rectangle12" style="width: 113px; height: 39px; left: 11px; top: 330px; position: absolute; background: #D9D9D9; border-radius: 40px"></div>
+        <div class="AddToCart" style="left: 30px; top: 338px; position: absolute; text-align: center; color: black; font-size: 16px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">add to cart</div>
+        <div class="IconHeartOutline" style="width: 52.84px; height: 53px; left: 356px; top: 309px; position: absolute">
+            <div class="Vector" style="width: 52.84px; height: 53px; left: 0px; top: 0px; position: absolute; opacity: 0; background: white">*</div>
+            <div class="Vector" style="width: 44.04px; height: 37.54px; left: 4.40px; top: 8.83px; position: absolute; background: transparent;color:white">*</div>
+        </div>
+    </div>
+    <div class="Frame5" style="left: 246px; top: 592px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+        <div class="Picture1" style="width: 418px; height: 395px; background: #252124"></div>
+        <img class="Unnamed3RemovebgPreview1" style="width: 297px; height: 238px; left: 47px; top: 0px; position: absolute" src="C:\xampp\htdocs\swe project\brown.png" />
+        <div class="BrownHoodie" style="left: 11px; top: 264px; position: absolute; text-align: center; color: #7F96B3; font-size: 25px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">brown hoodie</div>
+        <div class="Rectangle13" style="width: 113px; height: 39px; left: 11px; top: 330px; position: absolute; background: #D9D9D9; border-radius: 40px"></div>
+        <div class="AddToCart" style="left: 30px; top: 338px; position: absolute; text-align: center; color: black; font-size: 16px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">add to cart</div> <div class="IconHeartOutline" style="width: 52.84px; height: 53px; left: 345px; top: 321px; position: absolute">
+        <div class="Vector" style="width: 52.84px; height: 53px; left: 0px; top: 0px; position: absolute; opacity: 0; background: white">*</div>
+            <div class="Vector" style="width: 44.04px; height: 37.54px; left: 4.40px; top: 8.83px; position: absolute; background: transparent;color:white">*</div>
+        </div>
+    </div>
+    <div class="Frame6" style="left: 939px; top: 617px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+        <div class="Picture1" style="width: 418px; height: 374px; background: #252124"></div>
+        <img class="Unnamed3RemovebgPreview1" style="width: 297px; height: 238px; left: 47px; top: 0px; position: absolute" src="C:\xampp\htdocs\swe project\jeans.png" />
+        <div class="MiseryJeans" style="left: 12px; top: 246px; position: absolute; text-align: center; color: #7F96B3; font-size: 25px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">misery jeans</div>
+        <div class="Rectangle15" style="width: 113px; height: 39px; left: 11px; top: 330px; position: absolute; background: #D9D9D9; border-radius: 40px"></div>
+        <div class="AddToCart" style="left: 30px; top: 338px; position: absolute; text-align: center; color: black; font-size: 16px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">add to cart</div> <div class="IconHeartOutline" style="width: 52.84px; height: 53px; left: 342px; top: 303px; position: absolute">
+        <div class="Vector" style="width: 52.84px; height: 53px; left: 0px; top: 0px; position: absolute; opacity: 0; background: white">*</div>
+            <div class="Vector" style="width: 44.04px; height: 37.54px; left: 4.40px; top: 8.83px; position: absolute; background: transparent;color:white">*</div>
+        </div>
+    </div>
+    <div class="Unisex" style="left: 1307px; top: 127px; position: absolute; color: #7F96B3; font-size: 30px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">UNISEX<br/></div>
+    <div class="Navbar" style="width: 1383px; height: 62px; left: 29px; top: 34px; position: absolute">
+        <div class="About" style="left: 646px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">ABOUT</div>
+        <div class="Store" style="left: 800px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">STORE</div>
+        <div class="Contact" style="left: 949px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">CONTACT</div>
+        <div class="Home" style="left: 513px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">HOME</div>
+        <div class="Vector" style="width: 31.99px; height: 28.86px; left: 1116px; top: 8.57px; position: absolute; background: #7F96B3"></div>
+        <div class="Vector" style="width: 36px; height: 36px; left: 1114px; top: 5px; position: absolute"></div>
+        <div class="Misery" style="left: 0px; top: 0px; position: absolute; color: #7F96B3; font-size: 48px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">MISERY</div>
+        <div class="Vector" style="width: 30px; height: 30px; left: 1177.50px; top: 9px; position: absolute; background: #7F96B3"></div>
+        <div class="Register" style="left: 1282px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">REGISTER</div>
+    </div>
+</div>
+</body>
+</html>
