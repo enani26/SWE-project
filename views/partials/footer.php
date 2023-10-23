f48d880877c76250cae5839ca739ebc2e5adb856
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2023-10-23 03:51:04 +0300

    footer

---
+++ views/partials/footer.php
@@ -0,0 +1,62 @@
+<DOCTYPE html>
+    <html>
+        <head>
+            <meta name="viewport" content="width=device-width", initial-scale="1.0">
+            <title> Footer</title>
+            <link rel="stylesheet" href="footer.css">
+            <script src="https://kit.fontawesome.com/c4254e24a8.js"></script>
+        </head>
+        <body>
+            <footer>
+                <div class="row">
+                    <div class="col">
+                    <img src="logo.png" class="logo">
+                    <p> 
+                       Fashion refers to anything that becomes a rage among the masses. 
+                        Fashion is a popular aesthetic expression. 
+                        Most Noteworthy, it is something that is in vogue. 
+                        Fashion appears in clothing, footwear, accessories, makeup, hairstyles, lifestyle, and body proportions. 
+                       
+                    </p>
+                    </div>
+                    <div>
+                        <div class="col">
+                        <h3>Office <div class ="underline"><span></span></div></h3>
+                        <p> ITPL Road</p>
+                        <p>Cairo,Egypt</p>
+                        <p> Cairo,PIN 560066, Egypt</p>
+                        <p class="email-id">miseryegypt@outlook.com</p>
+                        <h4>+02 - 0123456789</h4>
+                        </div>
+                        <div class="col">
+                        <h3>Links <div class ="underline"><span></span></div></h3></h3>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Services</a></li>
+                            <li><a href="">About Us</a></li>
+                            <li><a href="">Features</a></li>
+                            <li> <a href="">Contacts</a></li>
+                        </ul>
+                        </div>
+                        <div classs="col">
+                        <h3>Newsletter <div class ="underline"><span></span></h3>
+                        <form>
+                            <i class="far fa-envelope"></i>
+                        <input type="email" placeholder="Enter your email id" required>
+                        <button type="submit"><i class="fas fa-arrow-right"></i></button>
+                        </form>
+                        </div>
+                        <div class="social-icons">
+                            <i class="fab fa-facebook-f"></i>
+                            <i class="fab fa-twitter"></i>
+                            <i class="fab fa-whatsapp"></i>
+                            <i class="fab fa-pinterest"> </i>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <p class="copyright">Easy Tutorials @ 2021 - All Rights Reserved</p>
+            </footer>
+        </body>
+    </html>
+</DOCTYPE>
