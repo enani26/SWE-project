da00128c87ff12df3ad95c3d574971ea92f0e80d
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2023-10-24 12:34:41 +0300

    about

---
+++ views/pages/aboutus.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="../../public/css/aboutus.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrap/4.3.1/css/bootstrap.min.css"> 
+    </head>
+    <body>
+        <section id="about">
+       <div class="about-1">
+        <h1>ABOUT US</h1>
+        <p>Clothes……ever since we began wrapping ourselves in leaves and animal skins, clothing has represented a very important part of our needs. So important it is, studies show that clothing ranks with food and shelter in a hierarchy of needs1.</p>
+       </div>
+       <div id="about-2">
+        <div class="content-box-lg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="about-item text-center">
+                       <i class="fa fa-book"></i>
+                       <h3>HISTORY</h3>
+                       <hr>
+                        <p> With more brands on the market than ever before, it is increasingly necessary and at the same time challenging for firms to differentiate their brands from competitors (Keller
+& Richey, 2006). Fashion brands in particular need to pay special attention to this matter because often there is little difference among many brands’ apparel products on the market. In addition, fashion brands are highly symbolic and self-expressive, reflecting a consumer’s lifestyle and values
+                        </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="about-item text-center">
+                            <i class="fa fa-globe"></i>
+                            <h3>VISION</h3>
+                            <hr>
+                            <p>With more brands on the market than ever before, it is increasingly necessary and at the same time challenging for firms to differentiate their brands from competitors (Keller
+                                & Richey, 2006). Fashion brands in particular need to pay special attention to this matter because often there is little difference among many brands’ apparel products on the market. In addition, fashion brands are highly symbolic and self-expressive, reflecting a consumer’s lifestyle and values
+                       </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="about-item text-center">
+                            <i class="fa fa-pencil"></i>
+                            <h3>ACHIEVEMENTS</h3>
+                            <hr>
+                            <p>With more brands on the market than ever before, it is increasingly necessary and at the same time challenging for firms to differentiate their brands from competitors (Keller
+                                & Richey, 2006). Fashion brands in particular need to pay special attention to this matter because often there is little difference among many brands’ apparel products on the market. In addition, fashion brands are highly symbolic and self-expressive, reflecting a consumer’s lifestyle and values
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       </div>
+        </section>
+        <footer class="text-center">
+            <p>copyright &copy; 2021 All rights reserved by freddie Nice Inc.</p>
+        </footer>
+    </body>
+</html>
