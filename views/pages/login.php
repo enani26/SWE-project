d29bd5062d53efacdac73ae791c75f4a065cb218
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2023-10-24 22:04:42 +0300

    loginnnn

---
+++ views/pages/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylee.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <div class="wrapper">
+        <span class="bg-animate"></span>
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form action="#">
+                <div class="input-box">
+                <input type="text" required >
+                <label>Firstname</label>
+                <i class='bx bxs-user'></i>
+                </div>
+
+                <div class="input-box">
+                    <input type="text" required >
+                    <label>Lastname</label>
+                    <i class='bx bxs-user'></i>
+                </div>
+
+                <div class="input-box">
+                <input type="password" required >
+                <label>Password</label>
+                <box-icon type='solid' name='lock-alt'></box-icon>
+                <i class='bx bxs-lock-alt'></i>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="logreg-link">
+                    <p>Don't have an account? <a href="#" class="register-link">Sign Up</a></p>
+                </div>
+
+
+
+
+            </form>
+        </div>
+<div class="info-text login">
+    <h2>Welcome Back!</h2>
+</div>
+
+
+    </div>
+</body>
+</html>
