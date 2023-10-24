58feb416e436bf74be653761f23fc22c08a5dc30
mohamed2106505 <140180824+mohamed2106505@users.noreply.github.com>
2023-10-24 21:58:53 +0300

    Update store.php

--- views/pages/store.php
+++ views/pages/store.php
@@ -89,30 +89,15 @@
 <body>
 
 
-    <div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
-
-        <div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
-            <a href="about.html"
-                style="left: 646px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">ABOUT</a>
-            <a href="store.php"
-                style="left: 800px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">STORE</a>
-            <a href="contact.html"
-                style="left: 949px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">CONTACT</a>
-            <a href="home.html"
-                style="left: 513px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">HOME</a>
-            <div
-                style="width: 31.99px; height: 28.86px; left: 1116px; top: 8.57px; position: absolute; color: #7F96B3; font-size: 25px;">
-                🛒</div>
-            <div
-                style="left: 0px; top: 0px; position: absolute; color: #7F96B3; font-size: 48px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">
-                MISERY</div>
-            <div
-                style="width: 33px; height: 32px; left: 1177.50px; top: 9px; position: absolute; color: #7F96B3;font-size: 25px;">
-                ♡</div>
-            <a href="register.html"
-                style="left: 1282px; top: 12px; position: absolute; color: #7F96B3;  font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">REGISTER</a>
-        </div>
-    </div>
+<nav class="navbar">
+    <a href="about.php" style="left: 560px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word" class="nav-item">CART</a>
+    <a href="store.php" style="left: 681px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word" class="nav-item">STORE</a>
+    <a href="contact.php" style="left: 819px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word" class="nav-item">WISHLIST</a>
+    <a href="index.php" style="left: 416px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word" class="nav-item">HOME</a>
+    <div class="nav-highlight"></div>
+    <div style="left: 50px; top: 5px; position: absolute; color: #7F96B3; font-size: 48px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">MISERY</div>
+    <a href="register.php" style="left: 1154px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word" class="nav-item">JOIN-US</a>
+</nav>
     
     <div class="sortt">
         <select>
