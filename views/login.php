64841ea01800d6fcd141b15607cb772ef2477c4a
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2023-10-24 16:29:22 +0300

    reg

--- views/login.php
+++ views/login.php
@@ -1,67 +1,44 @@
-<!-- here will be the sign up form-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="registration.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-        <div class="container">
-            <div class="box form-box">
-
-                <header>Sign in</header><br>
-                <form action="" method="post">
-                    <div class="field input">
-                        <label for="username">Username</label>
-                        <input type="text" name="username" id="username" required>
-                    </div>
-
-                    <div class="field input">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" id="password" required>
-                    </div>
-
-                    <div class="field">
-                        
-                        <input type="submit" class="btn" name="submit" value="Login" required>
-                    </div>
-                    <div class="links">
-                        Don't have an account? <a href="register.html">Sign Up Now</a>
-
-                    </div>
-                </form>
-
-            
+    <div class="wrapper">
+        <span class="bg-animate"></span>
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form action="#">
+                <div class="input-box">
+                <input type="text" required >
+                <label>Username</label>
+                <i class='bx bxs-user'></i>
+                </div>
+                <div class="input-box">
+                <input type="password" required >
+                <label>Password</label>
+                <box-icon type='solid' name='lock-alt'></box-icon>
+                <i class='bx bxs-lock-alt'></i>
+                </div>
+                <button type="submit" class="btn">Login</button>
+                <div class="logreg-link">
+                    <p>Don't have an account? <a href="#" class="register-link">Sign Up</a></p>
+                </div>
 
 
+
+
+            </form>
         </div>
-        </div>
+<div class="info-text login">
+    <h2>Welcome Back!</h2>
+</div>
+
+
+    </div>
 </body>
 </html>
-
-
-    <form>
-    <div class="form-group">
-        <label for="firstname" class="form-label">First Name</label>
-        <input type="text" class="form-control" id="firstname" placeholder="Enter First Name">
-    </div>
-
-
-    <div class="form-group">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-    </div>
-    <div class="form-group">
-        <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
-    </div>
-    <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-    </form><!--el id ashan lama adoos ala kelmet password hy3mel highlight-->
