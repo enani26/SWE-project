847477d160395f3406c71652067767f11aac13aa
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2023-10-25 19:34:47 +0300

    aaddmin

--- views/pages/admin.php
+++ views/pages/admin.php
@@ -6,10 +6,10 @@
     <link rel="stylesheet" href="../public/css/admin.css">
     <title>Admin Dashboard</title>
 
-    <!-- Montserrat Font -->
+    <!-- Font -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-    <!-- Material Icons -->
+    <!-- Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
 
     <!-- Custom CSS -->
