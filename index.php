60f182b3717a3f20274b1620e9870f1717780a75
MazenFouad <125610746+MazenFouad@users.noreply.github.com>
2023-10-25 23:58:41 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
 </head>
+
 <body>
 
 
-     
-    <div style="width: 100%; height: 100%; position: relative; background: black">
-        <div style="width: 1440px; height: 1024px; left: 0px; top: 0px; position: absolute"></div>
-        <div style="width: 1443px; height: 1023px; left: -2px; top: 7px; position: absolute">
-            <img src="./images/image2.jpg" style="width: 100%; height: 100vh; left: 0px; top: 0px; position: absolute"  />
+<div style="width: 100%; height: 4015px; left: 0px; top: 0px; position: absolute; background:#252124"></div>
+<div style="width: 1383px; height: 62px; left: 28px; top: 36px; position: absolute">
+<?php include 'navbar.php';?>
 
-            
-        </div>
-        <div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
-            <div style="width: 1383px; height: 62px; left: 39px; top: 34px; position: absolute">
-                <a href="/about" style="left: 800px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">STORE</a>
-                <a href="/contact" style="left: 949px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">CONTACT</a>
-                <a href="/index" style="left: 513px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">HOME</a>
-                <div style="width: 31.99px; height: 28.86px; left: 1116px; top: 8.57px; position: absolute; color: #7F96B3; font-size: 25px;"> 🛒</div>
-                <div style ="left: 0px; top: 0px; position: absolute; color: #7F96B3; font-size: 48px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">MISERY</div>
-                <div style="width: 33px; height: 32px; left: 1177.50px; top: 9px; position: absolute; color: #7F96B3;font-size: 25px;"> ♡</div>
-                <a href="/register" style="left: 1282px; top: 12px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">REGISTER</a>
-            </div>
-            
-        </div>
-        <div style="width: 648px; height: 320.72px; left: 39px; top: 243px; position: absolute">
-            <div style="width: 257px; height: 57px; left: 159px; top: 189px; position: absolute; background: rgba(127, 150, 179, 0.60); box-shadow: 0px 0px 25px rgba(101, 153, 32, 0.25); border-radius: 40px"></div>
-            <div style="width: 638px; left: 10px; top: 0px; position: absolute"><span style="color: #7F96B3; font-size: 36px; font-family: Montaga; font-weight: 400; text-transform: capitalize; word-wrap: break-word">MISERY</span><span style="color: #7F96B3; font-size: 36px; font-family: Montserrat; font-weight: 700; text-transform: capitalize; word-wrap: break-word"> Streetwear, Unleash Your Inner Edge!</span></div>
-            <div style="width: 630px; left: 3px; top: 108px; position: absolute; text-align: center; color: #7F96B3; font-size: 18px; font-family: Montserrat; font-weight: 700; text-transform: lowercase; word-wrap: break-word">"Dive into the raw, authentic world of Misery Streetwear – where urban edge meets individuality.”</div>
-            <div style="left: 557.42px; top: 197px; position: absolute; transform: rotate(27.65deg); transform-origin: 0 0; text-align: center; color: #7F96B3; font-size: 19px; font-family: Gochi Hand; font-weight: 400; text-transform: lowercase; word-wrap: break-word">order from<br/> here</div>
-            <div style="width: 24.55px; height: 16.27px; left: 394.27px; top: 207px;font-size: 25px; position: absolute; transform: rotate(89.48deg); transform-origin: 0 0; color:black">➤</div>
-            <a href="/store" style="left: 177px; top: 203px; position: absolute; text-align: center; color: black; font-size: 24px; font-family: Montserrat; font-weight: 700; text-transform: lowercase; word-wrap: break-word">Start Shopping</a>
-        </div>
-    </div>
-   
-  
-    
+<img style="width: 675px; height: 872px; left: 709px; top: 180px; position: absolute; border-top-left-radius: 8px; border-top-right-radius: 8px; border-bottom-right-radius: 8px; border-bottom-left-radius: 270px" src="./img/pic2.jpg"/>
+<div style="width: 720px; height: 516px; left: 155px; top: 254px; position: absolute">
+<div style="left: 0px; top: 0px; position: absolute; color: #7F96B3; font-size: 65px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">EMBRACE THE<br/> DARKNESS<br/> inside you</div>
+<div style="width: 479px; height: 101px; left: 0px; top: 258px; position: absolute; color: #7F96B3; font-size: 18px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">Misery is more than just a clothing brand; it's an expression of individuality. Our designs capture the essence of local culture and creativity, providing you with a unique style that stands out in the crowd.</div>
+
+<div style="width: 194px; height: 74px; left: 137px; top: 422px; position: absolute; background: #020202"></div>
+</div>
+<a href="store.php">
+    <button style="width: 205px; left: 290px; top: 701px; position: absolute; color: #7F96B3; font-size: 18px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word; background: transparent; border: none; cursor: pointer;">SHOP NOW</button>
+</a>
+
+<div style="left: 464px; top: 1091px; position: absolute; color: #7F96B3; font-size: 65px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">new collection</div>
+<div style="width: 423px; left: 741px; top: 1738px; position: absolute; color: #7F96B3; font-size: 50px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">Best fashion since 2021</div>
+<div style="width: 691px; left: 367px; top: 1174px; position: absolute; color: #7F96B3; font-size: 24px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">Unveiling the Essence of Misery's Latest Collection</div>
+<img style="width: 288px; height: 262px; left: 251px; top: 1252px; position: absolute" src="./img/pic4.jpg" />
+<img style="width: 288px; height: 264px; left: 944px; top: 1252px; position: absolute" src="./img/pic 5.jpg" />
+<div style="width: 288px; height: 57px; left: 251px; top: 1514px; position: absolute; background: #D9D9D9"></div>
+<div style="width: 288px; height: 57px; left: 944px; top: 1514px; position: absolute; background: #D9D9D9"></div>
+<div style="left: 278px; top: 1528px; position: absolute; color: black; font-size: 24px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">Misery camo shorts</div>
+<div style="left: 977px; top: 1528px; position: absolute; color: black; font-size: 24px; font-family: Montserrat; font-weight: 400; word-wrap: break-word">Misery Jeans shorts</div>
+<img style="width: 480px; height: 750px; left: 155px; top: 1738px; position: absolute; border-radius: 156px" src="./img/pic1.jpg" />
+<div style="width: 532px; left: 741px; top: 1886px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Montagu Slab; font-weight: 400; word-wrap: break-word">Misery's fashion isn't just about looking good; it's about feeling confident and expressing your unique self. Our collections empower you to embrace your individuality and stand out in a crowd.</div>
+<div style="width: 1440px; height: 930px; left: 0px; top: 2712px; position: absolute; background: #252124"></div>
+<div style="left: 155px; top: 2788px; position: absolute; color: #7F96B3; font-size: 64px; font-family: Montagu Slab; font-weight: 700; word-wrap: break-word">Best Seller <br/>Product</div>
+<img style="width: 500px; height: 481px; left: 690px; top: 2788px; position: absolute" src="./img/pic 6.jpg" />
+<div style="width: 503px; height: 192px; left: 690px; top: 3269px; position: absolute; background: #504C4C"></div>
+<div style="left: 721px; top: 3375px; position: absolute">
+<span style="color: white; font-size: 18px; font-family: Moul; font-weight: 400; text-decoration: line-through; word-wrap: break-word">950 le</span><span style="color: white; font-size: 18px; font-family: Moul; font-weight: 400; word-wrap: break-word"> 800 le</span></div>
+
+<div style="left: 721px; top: 3332px; position: absolute; color: white; font-size: 18px; font-family: Moul; font-weight: 400; word-wrap: break-word">Misery Jeans</div>
+<div style="width: 398px; height: 185px; left: 155px; top: 2969px; position: absolute; color: #7F96B3; font-size: 20px; font-family: Moul; font-weight: 400; word-wrap: break-word">Misery jeans are synonymous with style that stands the test of time. From classic cuts to contemporary designs, there's a pair for every fashion preference.</div>
+</div>
+
+
+
 </body>
 </html>
+
+
