96e3a03cc189042f326cec6ee509c3cab05a63f5
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2023-10-26 06:25:15 +0300

    edit in index

--- index.php
+++ index.php
@@ -5,23 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 </head>
-<?php
-session_start();
 
-if(!empty($_SESSION['ID'])) {
-	echo "<h1>Welcome ".$_SESSION['firstname']."</h1>";
-	echo"<a href='View.php'>View Profile</a><br><br>";
-	echo"<a href='Edit.php'>Edit Profile</a><br><br>";
-	echo"<a href='Delete.php'>Delete Account</a><br><br>";
-	echo"<a href='SignOut.php'>SignOut Here</a>";
-}
-else{
-	echo "<h1>Welcome</h1>";
-	echo"<a href='login.php'>Login</a>";
-	echo"<br>";
-	echo"<br><a href='register.php'>Signup Here</a>";
-}
-?>
 <body>
 
 
