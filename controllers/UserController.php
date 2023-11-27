5b9f35a3b0c38ee71858734b5acb1e56325f3b68
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2023-11-27 15:20:17 +0200

    committtt

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -30,5 +30,47 @@ include_once "models/UserModel.php";
             $isvalid=false;
         }
     }
+    if(empty($_POST["email"])){
+        $EmailErr="Email is required";
+        $isvalid=false;
+    }
+    else if(!filter_var($_POST["email"],FILTER_VALIDATE-EMAIL)){
+        $EmailErr="invalid email format";
+        $isvalid=false;
+    }
+      if(empty($_POST["password"]))  {
+        $PassErr="Password id Reuired";
+        $isvalid=false;
+      }
+      else if(strlen($_POST["Password"])<10){
+        $passErr="Password must be at least 10 characters long";
+        $isvalid=false;
+      }
+      if(empty($_POST["confirmpassword"])){
+        $ConfirmPassErr="confirm password is Required";
+        $isvalid=false;
+      }
+      else if (!preg_match($confirmpassword,$_POST("confirmpassword"))){
+        $ConfirmPassErr="Invalid Password";
+        $isvalid=false;
+      }
+      else if(!preg_match($confirmpassword,$_POST["password"])){
+        $ConfirmPassErr="Invalid Password";
+        $isvalid=false;
+      }
+      }
+      if(empty($_POST["phone"])){
+        $PhoneErr="Phone number is required";
+        $isvalid=false;
+      }
+      else{
+        //Exression for a valid 10-digit phone number
+        $phoneRegex='/^0\d{10}$/';
+        if(!preg_match($phoneRegex,$_POST["phone"])){
+            $PhoneErr="invalid phone number";
+            $isvalid=false;
+        }
+      }
+      $user= new user
  }
 }
