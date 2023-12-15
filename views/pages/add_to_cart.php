47e6aa78d5882536c3139f586c2342bbbc9ea878
MazenFouad <125610746+MazenFouad@users.noreply.github.com>
2023-12-15 16:46:09 +0200

    Update add_to_cart.php

--- views/pages/add_to_cart.php
+++ views/pages/add_to_cart.php
@@ -1,12 +1,8 @@
-<!-- add_to_cart.php -->
 <?php
-// Include necessary files and initialize session
 session_start();
-require_once('../../models/product.php'); // Assuming your Product class is in a file named Product.php
+require_once('../../models/product.php'); 
 
-// Function to fetch product details using your Product class
 function fetchProductDetails($product_id) {
-    // Implement your database connection logic here
     global $conn;
     $sql = "SELECT * FROM store_products WHERE product_id = $product_id";
     $result = mysqli_query($conn, $sql);
@@ -27,23 +23,18 @@ function fetchProductDetails($product_id) {
     return null;
 }
 
-// Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_to_cart'])) {
     $product_id = $_POST['product_id'];
 
-    // Initialize the cart array if it doesn't exist
     if (!isset($_SESSION['cart'])) {
         $_SESSION['cart'] = [];
     }
 
-    // Check if the product is already in the cart
     $product_index = array_search($product_id, array_column($_SESSION['cart'], 'product_id'));
 
     if ($product_index !== false) {
-        // If the product is already in the cart, increment the quantity
         $_SESSION['cart'][$product_index]['quantity'] += 1;
     } else {
-        // If the product is not in the cart, fetch product details and add it with quantity 1
         $product = fetchProductDetails($product_id);
 
         if ($product) {
@@ -55,8 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_to_cart'])) {
         }
     }
 
-    // Optionally, you can redirect the user to the product page or wherever you want
-    header("Location: products.php");
+   // header("Location: store.php");
     exit();
 }
 ?>
