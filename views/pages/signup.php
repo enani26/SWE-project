d0e017637d3738a11ba9e375cdb52a33cb4503c1
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2023-12-18 03:16:34 +0200

    footerrr

--- views/pages/signup.php
+++ views/pages/signup.php
@@ -8,6 +8,7 @@
 </head>
 <body>
 <?= include '../partials/navbar.php' ?>
+<?= include '../partials/footer.php' ?>
   <div class="container">
     <h2>Registration Form</h2>
     <form action="register.php" method="POST">
