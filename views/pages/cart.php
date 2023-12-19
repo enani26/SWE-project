520c2f8b52949434638370af9045011d559e7014
MazenFouad <125610746+MazenFouad@users.noreply.github.com>
2023-12-19 04:19:34 +0200

    Update cart.php

--- views/pages/cart.php
+++ views/pages/cart.php
@@ -15,53 +15,73 @@
         </div>
 
         <?php
-        session_start();
-        require_once('../../models/product.php'); 
+session_start();
+require_once('../../models/product.php'); 
 
-        function fetchProductDetails($product_id) {
-            global $conn;
-            $sql = "SELECT * FROM store_products WHERE product_id = $product_id";
-            $result = mysqli_query($conn, $sql);
+function fetchProductDetailsByName($product_name) {
+    global $conn;
 
-            if ($result && mysqli_num_rows($result) > 0) {
-                $row = mysqli_fetch_assoc($result);
-                return new Product(
-                    $row['product_id'],
-                    $row['product_name'],
-                    $row['product_description'],
-                    $row['product_keyword'],
-                    $row['product_img'],
-                    $row['product_price'],
-                    $row['status']
-                );
-            }
+    // Ensure $conn is a valid MySQLi connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
-            return null;
+    $safe_product_name = mysqli_real_escape_string($conn, $product_name);
+    $sql = "SELECT * FROM store_products WHERE product_name = '$safe_product_name'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        return new Product(
+            $row['product_id'],
+            $row['product_name'],
+            $row['product_description'],
+            $row['product_keyword'],
+            $row['product_img'],
+            $row['product_price'],
+            $row['status']
+        );
+    }
+
+    return null;
+}
+
+function addToCart($product) {
+    if (!isset($_SESSION['cart'])) {
+        $_SESSION['cart'] = [];
+    }
+
+    // Check if the product is already in the cart
+    $product_index = array_search($product->getId(), array_column($_SESSION['cart'], 'product_id'));
+
+    if ($product_index !== false) {
+        // Product is already in the cart, increase quantity
+        $_SESSION['cart'][$product_index]['quantity'] += 1;
+    } else {
+        // Product is not in the cart, add it
+        $_SESSION['cart'][] = [
+            'product_id' => $product->getId(),
+            'quantity' => 1,
+            'product' => $product
+        ];
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_to_cart'])) {
+    if (isset($_POST['product_name'])) {
+        $product_name = $_POST['product_name'];
+        $product = fetchProductDetailsByName($product_name);
+
+        if ($product) {
+            addToCart($product);
+            echo "Product added to cart.";
+        } else {
+            echo "Product not found.";
         }
-       
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['add_to_cart'])) {
-            $product_id = $_POST['product_id'];
-
-            if (!isset($_SESSION['cart'])) {
-                $_SESSION['cart'] = [];
-            }
-
-            $product_index = array_search($product_id, array_column($_SESSION['cart'], 'product_id'));
-            if ($product_index !== false) {
-                $_SESSION['cart'][$product_index]['quantity'] += 1;
-            } else {
-                $product = fetchProductDetails($product_id);
-
-                if ($product) {
-                    $_SESSION['cart'][] = [
-                        'product_id' => $product->getId(),
-                        'quantity' => 1,
-                        'product' => $product
-                    ];
-                }
-            }
-        }
+    } else {
+        echo "Product name not set.";
+    }
+}
 
 
         if (!empty($_SESSION['cart'])) {
