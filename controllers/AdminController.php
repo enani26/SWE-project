b3de58f513ef05123097c0cec837a359b3a3b45b
aya2372002 <125696175+aya2372002@users.noreply.github.com>
2023-12-19 04:57:09 +0200

    adminnnn

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,8 +1,12 @@
 <?php
 require_once("controller.php")
-
+require_once("products.php")
 class AdminController extends Controller 
 {
+    private $productsModel;
+    public function __construct() {
+        $this->productsModel = new products();
+    }
     public function insert(){ 
 		$firstname = $_REQUEST['firstname'];
         $lastname=$_REQUEST['lastname'];
@@ -15,15 +19,55 @@ class AdminController extends Controller
 
     public function EditAdmin(){
             $firstname = $_REQUEST['firstname'];
-            $lastname=$_REUEST['lastname'];
+            $lastname=$_REQUEST['lastname'];
             $email=$_REQUEST['email'];
             $password = $_REQUEST['password'];
             $phone = $_REQUEST['phone'];
-            $Usertype=$_REQUEST['UserType']
+            $Usertype=$_REQUEST['UserType'];
             $this->model->editAdmin($firstname,$lastname,$email,$password,$phone,$Usertype);
         }
     public function DeletAdmin(){
             $this->model->deletAdmin();
+        }
+        if (isset($_POST['insert_product'])) {
+            $productName = $_POST['product_name'];
+            $productDescription = $_POST['product_description'];
+            $productKeyword = $_POST['product_keyword'];
+            $productImg = $_POST['product_img'];
+            $productPrice = $_POST['product_price'];
+            $status = $_POST['status'];
+            
+            $this->productsModel->insertProduct($productName, $productDescription, $productKeyword, $productImg, $productPrice, $status);
+            $this->redirectToSuccessPage();
+        }
+        if (isset($_POST['editproduct'])) {
+            $productName = $_POST['product_name'];
+            $productDescription = $_POST['product_description'];
+            $productKeyword = $_POST['product_keyword'];
+            $productImg = $_POST['product_img'];
+            $productPrice = $_POST['product_price'];
+            $status = $_POST['status'];
+
+            $this->productsModel->editProduct($productName, $productDescription, $productKeyword, $productImg, $productPrice, $status);
+            $this->redirectToSuccessPage();
+        }
+    
+        if (isset($_POST['delete_product'])) {
+            $productName = $_POST['product_name'];
+            $productDescription = $_POST['product_description'];
+            $productKeyword = $_POST['product_keyword'];
+            $productImg = $_POST['product_img'];
+            $productPrice = $_POST['product_price'];
+            $status = $_POST['status'];
+            
+            $this->productsModel->delete_Product($productName, $productDescription, $productKeyword, $productImg, $productPrice, $status);
+            $this->redirectToSuccessPage();
+        }
+        
+    private function redirectToSuccessPage() {
+            
+            header("Location:home.php");
+            exit();
         }
 
     }
--- views/pages/admin.php
+++ views/pages/admin.php
@@ -1,153 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" href="../../public/css/admindashboard.css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="../../public/css/admindashboard.css">
+</head>
+<body>
+    <h1>Admin Dashboard</h1>
 
-    <!-- Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Add Product Form -->
+    <form action="AdminController.php" method="post">
+        <label for="product_name">Product Name:</label>
+        <input type="text" id="product_name" name="product_name" required>
+        <button type="submit" name="add_product">Add Product</button>
+    </form>
 
-    <!-- Icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
+    <!-- Edit Product Form -->
+    <form action="AdminController.php" method="post">
+        <label for="edit_product_name">Edit Product Name:</label>
+        <input type="text" id="edit_product_name" name="edit_product_name" required>
+        <button type="submit" name="edit_product">Edit Product</button>
+    </form>
 
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="../../../public/css/admin.css">
-    
-  </head>
-  <body>
-    <div class="grid-container">
+    <!-- Delete Product Button -->
+    <button onclick="confirmDelete()">Delete Product</button>
 
-      <!-- Header -->
-      <header class="header">
-        <div class="menu-icon" onclick="openSidebar()">
-          <span class="material-icons-outlined">menu</span>
-        </div>
-        <div class="header-left">
-          <span class="material-icons-outlined">search</span>
-        </div>
-        <div class="header-right">
-          <span class="material-icons-outlined">notifications</span>
-          <span class="material-icons-outlined">email</span>
-          <span class="material-icons-outlined">account_circle</span>
-        </div>
-      </header>
-      <!-- End Header -->
-
-      <!-- Sidebar -->
-      <aside id="sidebar">
-        <div class="sidebar-title">
-          <div class="sidebar-brand">
-            <span class="material-icons-outlined">Misery</span> Misery's inventory
-          </div>
-          <span class="material-icons-outlined" onclick="closeSidebar()">close</span>
-        </div>
-
-        <ul class="sidebar-list">
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">dashboard</span> Dashboard
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">inventory_2</span> Products
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">fact_check</span> MISERY
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">add_shopping_cart</span> Purchase Orders
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">shopping_cart</span> Sales Orders
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">poll</span> Reports
-            </a>
-          </li>
-          <li class="sidebar-list-item">
-            <a href="#" target="_blank">
-              <span class="material-icons-outlined">settings</span> Settings
-            </a>
-          </li>
-        </ul>
-      </aside>
-      <!-- End Sidebar -->
-
-      <!-- Main -->
-      <main class="main-container">
-        <div class="main-title">
-          <p class="font-weight-bold">DASHBOARD</p>
-        </div>
-
-        <div class="main-cards">
-
-          <div class="card">
-            <div class="card-inner">
-              <p class="text-primary">PRODUCTS</p>
-              <span class="material-icons-outlined text-blue">inventory_2</span>
-            </div>
-            <span class="text-primary font-weight-bold">249</span>
-          </div>
-
-          <div class="card">
-            <div class="card-inner">
-              <p class="text-primary">PURCHASE ORDERS</p>
-              <span class="material-icons-outlined text-orange">add_shopping_cart</span>
-            </div>
-            <span class="text-primary font-weight-bold">83</span>
-          </div>
-
-          <div class="card">
-            <div class="card-inner">
-              <p class="text-primary">SALES ORDERS</p>
-              <span class="material-icons-outlined text-green">shopping_cart</span>
-            </div>
-            <span class="text-primary font-weight-bold">79</span>
-          </div>
-
-          <div class="card">
-            <div class="card-inner">
-              <p class="text-primary">MISERY ALERTS</p>
-              <span class="material-icons-outlined text-red">notification_important</span>
-            </div>
-            <span class="text-primary font-weight-bold">56</span>
-          </div>
-
-        </div>
-
-        <div class="charts">
-
-          <div class="charts-card">
-            <p class="chart-title">Top 5 Products</p>
-            <div id="bar-chart"></div>
-          </div>
-
-          <div class="charts-card">
-            <p class="chart-title">Purchase and Sales Orders</p>
-            <div id="area-chart"></div>
-          </div>
-
-        </div>
-      </main>
-      <!-- End Main -->
-
-    </div>
-
-    <!-- Scripts -->
-    <!-- ApexCharts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.35.3/apexcharts.min.js"></script>
-    <!-- Custom JS -->
-    <script src="js/scripts.js"></script>
-  </body>
+    <script>
+        function confirmDelete() {
+            var confirmation = confirm("Are you sure you want to delete this product?");
+            if (confirmation) {
+                //get the product id
+                var product_id="your product id here"
+               
+                //send a delete request using fetch
+                fetch('AdminController.php', {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ delete_product: true, product_id_to_delete: product_id }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    // Handle the response data if needed
+                    console.log(data);
+                    // Optionally, reload the page or update the UI
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            }
+        }
+    </script>
+</body>
 </html>
