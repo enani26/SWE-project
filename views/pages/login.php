7711d656e8f4ece7a472bce593d743fa35d64a15
Toka2104550 <125702984+Toka2104550@users.noreply.github.com>
2023-12-19 21:00:41 +0200

    commit

--- views/pages/login.php
+++ views/pages/login.php
@@ -3,46 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../../../public/css/signup.css"> 
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Register</title>
+    <link rel="stylesheet" href="../../public/css/login.css">
+
+
 </head>
 <body>
-    <div class="wrapper">
-        <span class="bg-animate"></span>
-        <div class="form-box login">
-            <h2>Login</h2>
-            <form action="" method="post">
-                    <div class="input-box">
-                            <input type="text" name="email" required >
-                            <label for="email">Email Address</label>
-                            <i class='bx bx-envelope' ></i>
+    <div class="hero">
+        <div class="form-box">
+            <div class="button-box">
+                <div id="btn"></div>
+                <button type="button" class="toggle-btn" onclick="login()">Login</button>
+                <button type="button" class="toggle-btn" onclick="register()">Register</button>
+            </div>
 
-                    <div class="input-box">
-                    <input type="password" name="password" required >
-                    <label>Password</label>
-                    <box-icon type='solid' name='lock-alt'></box-icon>
-                    <i class='bx bxs-lock-alt'></i>
-                    </div>
-                    
-                    <button type="submit" class="btn">Login</button>
-                    <div class="logreg-link">
-                        <p>Don't have an account? <a href="register.php" class="register-link">Sign Up</a></p>
-                    </div>
+            <div class="social-icons">
+                <img src="facebook.png">
+                <img src="twitter.png">
+                <img src="instagram.png">
+            </div>
 
+            <form id="login" class="input-group">
+                <input type="text" class="input-field" placeholder="username" required>
+                <input type="text" class="input-field" placeholder="Enter Password" required>
+                <input type="checkbox" class="check-box"><span>Remember Password</span>
+                <button type="submit" class="submit-btn">Login</button>
+            </form>
 
-
-
+            <form id="register" class="input-group">
+                <input type="text" class="input-field" placeholder="username" required>
+                <input type="email" class="input-field" placeholder="email" required>
+                <input type="password" class="input-field" placeholder="password" required>
+                <input type="checkbox" class="check-box"><span>    I agree to terms & conditions</span>
+                <button type="submit" class="submit-btn">Register</button>
             </form>
         </div>
-<div class="info-text login">
-    <h2>Welcome Back!</h2>
-</div>
-
-
     </div>
 
-    <?php
+    <script>
+        var x = document.getElementById("login");
+        var y = document.getElementById("register");
+        var z = document.getElementById("btn");
+
+        function register() {
+            x.style.left = "-400px";
+            y.style.left = "50px";
+            z.style.left = "110px";
+        }
+
+        function login() {
+            x.style.left = "50px";
+            y.style.left = "450px";
+            z.style.left = "0";
+        }
+    </script>
+
+<?php
    session_start();
    include_once "../../includes/dbh.inc.php";
    //grab data from user and see if it exists in database
@@ -69,6 +85,6 @@ $db=new DBh();
 	}
    }
 ?>
-
 </body>
+
 </html>
